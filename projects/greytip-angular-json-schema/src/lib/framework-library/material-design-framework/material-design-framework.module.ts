ed677efba10fbe9fed9545605cfb4fd89d3af55a
Harshal Pawshekar <harshal.pawshekar@greytip.com>
2024-08-02 19:59:33 +0530

    run migration for version 9 material

--- projects/greytip-angular-json-schema/src/lib/framework-library/material-design-framework/material-design-framework.module.ts
+++ projects/greytip-angular-json-schema/src/lib/framework-library/material-design-framework/material-design-framework.module.ts
@@ -2,13 +2,25 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import {
-  MatAutocompleteModule, MatButtonModule, MatButtonToggleModule, MatCardModule,
-  MatCheckboxModule, MatChipsModule, MatDatepickerModule, MatExpansionModule,
-  MatFormFieldModule, MatIconModule, MatInputModule, MatNativeDateModule,
-  MatRadioModule, MatSelectModule, MatSliderModule, MatSlideToggleModule,
-  MatStepperModule, MatTabsModule, MatTooltipModule,
-} from '@angular/material';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTooltipModule } from '@angular/material/tooltip';
 export const ANGULAR_MATERIAL_MODULES = [
   MatAutocompleteModule, MatButtonModule, MatButtonToggleModule, MatCardModule,
   MatCheckboxModule, MatChipsModule, MatDatepickerModule, MatExpansionModule,
